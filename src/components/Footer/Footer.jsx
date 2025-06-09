4b2415af9fe45b1a76e10367fa9963c3cd274f90
Dacotah <deelcoding@outlook.com>
2025-06-08 21:41:22 -0400

    Feedback Fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,40 +6,42 @@ function Footer() {
   return (
     <footer className="footer">
       <ul className="footer__list">
-        <li className="footer__links">
-          <Link
-            to="/"
-            className="footer__link">
-            Home
-          </Link>
-          <Link
-            to="https://tripleten.com"
-            target="_blank"
-            className="footer__link">
-            TripleTen
-          </Link>
-        </li>
-        <li className="footer__socials">
-          <Link
-            to="https://github.com/deelcoding"
-            target="_blank"
-            className="footer__social">
-            <img
-              src={github}
-              alt="GitHub"
-              className="footer__icon"
-            />
-          </Link>
-          <Link
-            to="https://facebook.com"
-            target="_blank"
-            className="footer__social">
-            <div
-              className="footer__facebook"
-              role="img"
-              aria-label="Facebook"
-            />
-          </Link>
+        <li className="footer__top">
+          <div className="footer__links">
+            <Link
+              to="/"
+              className="footer__link">
+              Home
+            </Link>
+            <Link
+              to="https://tripleten.com"
+              target="_blank"
+              className="footer__link">
+              TripleTen
+            </Link>
+          </div>
+          <div className="footer__socials">
+            <Link
+              to="https://github.com/deelcoding"
+              target="_blank"
+              className="footer__social">
+              <img
+                src={github}
+                alt="GitHub"
+                className="footer__icon"
+              />
+            </Link>
+            <Link
+              to="https://facebook.com"
+              target="_blank"
+              className="footer__social">
+              <div
+                className="footer__facebook"
+                role="img"
+                aria-label="Facebook"
+              />
+            </Link>
+          </div>
         </li>
         <li className="footer__copyright">
           &copy; {new Date().getFullYear()} Supersite, Powered by News API
