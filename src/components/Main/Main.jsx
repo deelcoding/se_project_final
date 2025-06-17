0ffb10416af748cbdf1e67207c7f0b24b7e7bf3c
Dacotah <deelcoding@outlook.com>
2025-06-17 18:08:09 -0400

    Feedback fixes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -16,8 +16,8 @@ function Main({ onSignIn, onSignUp, onSearch,onRegistrationComplete }) {
         {/* <Navigation /> */}
       </div>
       <div className="main__search">
-        <h1 className="main__search_title">What's going on in the world?</h1>
-        <p className="main__search_description">
+        <h1 className="main__search-title">What's going on in the world?</h1>
+        <p className="main__search-description">
           Find the latest news on any topic and save them in your personal
           account.
         </p>
