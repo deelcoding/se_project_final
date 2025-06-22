567902becbc5b924aed8ae15e753e4fc730f5752
Dacotah <deelcoding@outlook.com>
2025-06-21 21:12:38 -0400

    Feedback fixes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,19 +2,15 @@ import SearchForm from "../SearchForm/SearchForm";
 import NewsCard from "../NewsCard/NewsCard";
 import "./Main.css";
 import Header from "../Header/Header";
-import Navigation from "../Navigation/Navigation";
 
-function Main({ onSignIn, onSignUp, onSearch,onRegistrationComplete }) {
+function Main({ onSignIn, onSignUp, onSearch, onRegistrationComplete }) {
   return (
     <main className="main">
-      <div className="main__header">
-        <Header
-          onSignUp={onSignUp}
-          onSignIn={onSignIn}
-          onRegistrationComplete={onRegistrationComplete}
-        />
-        {/* <Navigation /> */}
-      </div>
+      <Header
+        onSignUp={onSignUp}
+        onSignIn={onSignIn}
+        onRegistrationComplete={onRegistrationComplete}
+      />
       <div className="main__search">
         <h1 className="main__search-title">What's going on in the world?</h1>
         <p className="main__search-description">
